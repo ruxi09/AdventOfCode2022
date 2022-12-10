959e4297acb4c27edd53fae9a009a7a96764f9c3
Ruxi <ruxi@dyn3254-61.wlan.ic.ac.uk>
2022-12-10 13:29:08 +0000

    AOC Day 10 java

--- 10_Dec/java/src/Main.java
+++ 10_Dec/java/src/Main.java
@@ -3,6 +3,13 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
+    private static void printPixel(char[][] grid, int cycle, int x) {
+        var row = cycle / 40;
+        var column = cycle % 40;
+
+        grid[row][column] = x >= column - 1 && x <= column + 1 ? '#' : ' ';
+    }
+
     public static void main(String[] args) {
         try {
             File myObj = new File("src/input_radu.txt");
@@ -11,12 +18,15 @@ public class Main {
             int cycleCount = 0;
             int sum = 0;
             int x = 1;
+            char[][] grid = new char[6][40];
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 String[] arrData = data.split(" ");
                 //noop takes one cycle to complete
                 if(arrData[0].equals("noop")){
+                    printPixel(grid, cycleCount, x);
                     cycleCount ++;
+//                    Part 2:
                     if (cycleCount % 40 == 20){
                         sum += x * cycleCount;
                     }
@@ -26,7 +36,9 @@ public class Main {
                     int value = Integer.parseInt(arrData[1]);
                     int cyclesLeft = 2;
                     while(cyclesLeft > 0 ){
+                        printPixel(grid, cycleCount, x);
                         cycleCount ++;
+//                         Part1:
                         if (cycleCount % 40 == 20){
                             sum += x * cycleCount;
                         }
@@ -35,7 +47,16 @@ public class Main {
                     x += value;
                 }
             }
+            System.out.println("Part 1:");
             System.out.println(sum);
+
+            System.out.println("Part 2:");
+            for (var line: grid) {
+                for (var column: line) {
+                    System.out.print(column);
+                }
+                System.out.println();
+            }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
